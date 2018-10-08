9e003ebd2d205312f74a1dd58978da810940c2ac
Steve Liddle <liddle@byu.edu>
2018-10-08 14:14:28 -0600

    Finished Part 12, implement data source protocol on deck view controller.

---
+++ Rook Card Demo/Controller/RookDeckViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RookDeckViewController.swift
+//  Rook Card Demo
+//
+//  Created by Steve Liddle on 10/8/18.
+//  Copyright © 2018 IS 543. All rights reserved.
+//
+
+import UIKit
+
+class RookDeckViewController : UIViewController {
+
+    // MARK: - Constants
+
+    private struct Storyboard {
+        static let RookCardCellIdentifier = "RookCardCell"
+    }
+
+    // MARK: - Properties
+
+    private var deck = RookDeck()
+
+    // MARK: - Outlets
+
+}
+
+extension RookDeckViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Storyboard.RookCardCellIdentifier,
+                                                      for: indexPath)
+
+        if let rookCardCell = cell as? RookCardCell {
+            rookCardCell.rookCardView.card = deck[indexPath.row]
+            rookCardCell.rookCardView.setNeedsDisplay()
+        }
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return deck.count
+    }
+}
---
+++ Rook Card Demo/View/RookCardCell.swift
@@ -0,0 +1,16 @@
+//
+//  RookCardCell.swift
+//  Rook Card Demo
+//
+//  Created by Steve Liddle on 10/8/18.
+//  Copyright © 2018 IS 543. All rights reserved.
+//
+
+import UIKit
+
+class RookCardCell : UICollectionViewCell {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var rookCardView: RookCardView!
+}
